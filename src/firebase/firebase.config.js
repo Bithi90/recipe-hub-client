ae9f580f621916ba1f95c058145edc8c6d6bca6f
Bithi90 <bka3670@gmail.com>
2023-05-03 12:19:31 +0600

    second

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqQoHO5RjiR0KOjON51ZiaQiOY3rlKAok",
+  authDomain: "recipe-hub-c3383.firebaseapp.com",
+  projectId: "recipe-hub-c3383",
+  storageBucket: "recipe-hub-c3383.appspot.com",
+  messagingSenderId: "1089503436902",
+  appId: "1:1089503436902:web:476b4caed4aa4362fe4da7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
